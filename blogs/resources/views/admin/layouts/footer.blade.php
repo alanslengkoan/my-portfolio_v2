6f5a0207a9a4cd0c2232ba2d90820c116adb2b0c
alanlengkoan <alansaputralengkoan@gmail.com>
2022-08-01 16:40:04 +0800

    first commit

---
+++ blogs/resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer text-center">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                2017
+                <script type="text/javascript">
+                    var thunskrg = new Date().getFullYear();
+                    document.write("- " + thunskrg);
+                </script>
+                <em> Copyright &copy; Alan Saputra Lengkoan </em>
+            </div>
+        </div>
+    </div>
+</footer>
